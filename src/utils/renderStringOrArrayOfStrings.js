d6bfdd33b375a4929612c75325105ba555e48c8d
ohyde <oliverhyde@gmail.com>
2016-12-02 11:36:41 +1300

    Building up smarter component

--- src/utils/renderStringOrArrayOfStrings.js
+++ src/utils/renderStringOrArrayOfStrings.js
@@ -4,11 +4,13 @@ import { mapIndexed } from './ramdaUtils';
 export const renderStringOrArrayOfStrings = (toRender) => {
   if (_.isNil(toRender)) { return; }
 
+  if (_.isObject(toRender)) { return (<div>{toRender.content}</div>); }
+                                      
   if (_.isArray(toRender)) {
     return mapIndexed((item, i) => {
       return (<div classNames={item.classes} key={i}>{item.content}</div>);
     }, toRender);
   }
 
-  return (<div>{toRender.content}</div>);
+  return (<div>{toRender}</div>);
 }
