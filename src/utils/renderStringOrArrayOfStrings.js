257f532df17ff1372bdf64d9740aeb3b44716e73
ohyde <oliverhyde@gmail.com>
2016-12-08 11:40:44 +1300

    Smaller render functions

--- src/utils/renderStringOrArrayOfStrings.js
+++ src/utils/renderStringOrArrayOfStrings.js
@@ -4,20 +4,17 @@ import React from 'react';
 import R from 'ramda';
 import { mapIndexed } from './ramdaUtils';
 
+const isObjectWithContent = R.allPass([R.is(Object), R.has('content')]);
 
 export const renderStringOrArrayOfStrings = (toRender: any) => {
   return R.cond([
     [R.isNil, R.always(undefined) ],
     [R.isArrayLike, arrayRender],
-    [R.is(Object), objectRender],
+    [isObjectWithContent, objectRender],
     [R.T, stringRender]
   ])(toRender);
-}
+};
 
-const stringRender = (toRender: string) => { return (<div>{toRender}</div>) }
-const arrayRender = (toRender: Array<any>) => { return mapIndexed(renderStringOrArrayOfStrings, toRender); }
-const objectRender = (toRender: {content: string}) => {
-  if(!R.has('content', toRender)) { return; }
-
-  return (<div>{toRender.content}</div>);
-}
+const stringRender = (toRender: string) => { return (<div>{toRender}</div>) };
+const arrayRender = (toRender: Array<any>) => { return mapIndexed(renderStringOrArrayOfStrings, toRender); };
+const objectRender = (toRender: {content: string}) => { return (<div>{toRender.content}</div>); };
