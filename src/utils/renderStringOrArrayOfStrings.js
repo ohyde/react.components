73f564a1dceeafc828dd93239240cd0366ed2239
ohyde <oliverhyde@gmail.com>
2016-12-08 11:35:01 +1300

    Added flow types and standardized naming

--- src/utils/renderStringOrArrayOfStrings.js
+++ src/utils/renderStringOrArrayOfStrings.js
@@ -8,19 +8,15 @@ import { mapIndexed } from './ramdaUtils';
 export const renderStringOrArrayOfStrings = (toRender: any) => {
   return R.cond([
     [R.isNil, R.always(undefined) ],
-    [R.isArrayLike, mapRender],
-    [R.is(Object), renderObject],
+    [R.isArrayLike, arrayRender],
+    [R.is(Object), objectRender],
     [R.T, stringRender]
   ])(toRender);
 }
 
-const stringRender = (toRender: any) => { return (<div>{toRender}</div>) }
-
-const mapRender = (toRender: any) => {
-  return mapIndexed(renderStringOrArrayOfStrings, toRender);
-}
-
-const renderObject = (toRender: any) => {
+const stringRender = (toRender: string) => { return (<div>{toRender}</div>) }
+const arrayRender = (toRender: Array<any>) => { return mapIndexed(renderStringOrArrayOfStrings, toRender); }
+const objectRender = (toRender: {content: string}) => {
   if(!R.has('content', toRender)) { return; }
 
   return (<div>{toRender.content}</div>);
