4b34903f42dbf5af95c9ef51052b150d765bcd78
ohyde <oliverhyde@gmail.com>
2016-12-30 10:37:10 +1300

    point free renderText, moved renderTypes into own folder

--- src/utils/renderText.js
+++ src/utils/renderText.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import R from 'ramda';
+import { stringRender, arrayRender, objectRender } from './renderTypes';
 
-export const renderText = (toRender) => {
-  return R.cond([
-    [R.is(String), stringRender],
-    [R.isArrayLike, arrayRender],
-    [R.allPass([R.is(Object), R.has('text')]), objectRender],
-    [R.T, R.always(undefined)]
-  ])(toRender);
-};
-
-export const stringRender = (toRender) => { return (<div>{toRender}</div>) };
-export const arrayRender = R.map(renderText);
-
-export const objectRender = (toRender) => {
-  return R.cond([
-    [R.has('element'), objectRenderWithElement],
-    [R.T, objectRenderInDiv]
-  ])(toRender);
-};
-export const objectRenderInDiv = (toRender) => {
-  return (
-    <div className={toRender.classes}>
-      {toRender.text}
-    </div>
-  );
-}
-export const objectRenderWithElement = (toRender) => {
-  return (
-    <toRender.element className={toRender.classes}>
-      {toRender.text}
-    </toRender.element>
-  );
-}
+export const renderText = R.cond([
+  [R.isNil, R.always(undefined)],
+  [R.is(String), stringRender],
+  [R.isArrayLike, arrayRender],
+  [R.allPass([R.is(Object), R.has('text')]), objectRender],
+  [R.T, R.always(undefined)]
+]);
---
+++ src/utils/renderTypes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import R from 'ramda';
+import { renderText } from './renderText';
+
+export const stringRender = (toRender) => { return (<div>{toRender}</div>) };
+export const arrayRender = (toRender) => { return R.map(renderText, toRender); };
+
+export const objectRender = (toRender) => {
+  return R.cond([
+    [R.has('element'), objectRenderWithElement],
+    [R.T, objectRenderInDiv]
+  ])(toRender);
+};
+
+export const objectRenderInDiv = (toRender) => {
+  return (
+    <div className={toRender.classes}>
+      {toRender.text}
+    </div>
+  );
+}
+
+export const objectRenderWithElement = (toRender) => {
+  return (
+    <toRender.element className={toRender.classes}>
+      {toRender.text}
+    </toRender.element>
+  );
+}
