8cf644fe83ea3c6003207e941ee7f51b58d2ca3e
Oliver Hyde <oliverhyde@gmail.com>
2017-01-03 11:59:44 +1300

    cleaned up util files

--- src/utils/ramdaUtils.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-
-import R from 'ramda';
-
-export const mapIndexed = R.addIndex(R.map);
-export const defaultToUndefined = R.defaultTo(undefined);
--- src/utils/renderText.js
+++ src/utils/renderText.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import R from 'ramda';
 import { stringRender, arrayRender, objectRender } from './renderTypes';
 
